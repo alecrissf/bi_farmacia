6f1e36be06e3abe5fb5a2c17559326b1c4571c54
Alessandra Cristina <alecrissf@proton.me>
2025-07-07 16:52:09 -0300

    feat: inicio da interface do BI

--- apps/frontend/src/bi/BiApp.tsx
+++ apps/frontend/src/bi/BiApp.tsx
@@ -1,90 +1,34 @@
-import {
-  ResponsiveContainer,
-  LineChart,
-  CartesianGrid,
-  XAxis,
-  YAxis,
-  Tooltip,
-  Legend,
-  Line,
-} from 'recharts';
-
-const data = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
+import './index.css';
 
 export function BiApp() {
   return (
-    <div>
-      <h1>BI App</h1>
-      <p>Teste usando a biblioteca de gráficos:</p>
-      <ResponsiveContainer width="100%" height={400}>
-        <LineChart
-          width={500}
-          height={300}
-          data={data}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line
-            type="monotone"
-            dataKey="pv"
-            stroke="#8884d8"
-            activeDot={{ r: 8 }}
-          />
-          <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-        </LineChart>
-      </ResponsiveContainer>
+    <div className="bi-wrapper">
+      <div className="side-bar">
+        <div className="info"></div>
+        <div className="filter-wrapper">
+          <div className="item">
+            <span>Categoria</span>
+            <select name="categoria-select" id="categoria-select" multiple>
+              <option value="aaa">aa</option>
+            </select>
+          </div>
+        </div>
+      </div>
+      <div className="content">
+        <div className="graph-container">
+          <h3>Total de Vendas</h3>
+          <b>R$ 100 K</b>
+        </div>
+
+        <div className="graph-container">
+          <h3>Quantidade de Vendas</h3>
+          <b>10 K</b>
+        </div>
+
+        <div className="graph-container">
+          <h3>Total de Vendas por Categoria</h3>
+        </div>
+      </div>
     </div>
   );
 }
